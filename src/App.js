442df1402e775e8bf45a5227a29fd3062bfb2ff9
moontracer <themoontracer@gmail.com>
2020-11-20 14:24:44 -0500

    basic routing now functional

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import {hot} from "react-hot-loader";
+import { BrowserRouter as Router } from "react-router-dom";
+import Nav from './Nav.js';
 
 class App extends Component {
     render(){
         return (
             <div className="App">
-                <h1>Hello Test!</h1>
+                <Router>
+                    <Nav />
+                </Router>
             </div>
         )
     }
