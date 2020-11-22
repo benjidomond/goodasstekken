5d5fdc3a0f39dc08170941b21832d50e220191d9
moontracer <themoontracer@gmail.com>
2020-11-21 19:52:49 -0500

    created views, add route config to app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import "./App.css";
 import {hot} from "react-hot-loader";
 import { BrowserRouter as Router } from "react-router-dom";
 import Nav from './Nav.js';
+import AccountPage from './views/accountPage.js';
+import AdminPage from './views/adminPage.js';
+import HomePage from './views/homePage.js';
+import LandingPage from './views/landingPage.js';
+import PasswordForgetPage from './views/passwordForgetPage.js';
+import SignInPage from './views/signInPage.js';
+import SignUpPage from './views/signUpPage.js';
 
 class App extends Component {
     render(){
---
+++ src/views/accountPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AccountPage extends Component {
+    render(){
+        return (
+            <div>
+            <h1>Account Page component!</h1>
+            </div>
+        )
+    }
+}
+
+export default AccountPage;
