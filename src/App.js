1d9e5bc4739360e5abe43537bb771fdb213e8b4d
moontracer <themoontracer@gmail.com>
2020-11-22 18:56:13 -0500

    added firebase config setings, adding dotenv webpack tomorrow

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import {hot} from "react-hot-loader";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Nav from './Nav.js';
 import AccountPage from './views/accountPage.js';
 import AdminPage from './views/adminPage.js';
@@ -10,6 +10,7 @@ import LandingPage from './views/landingPage.js';
 import PasswordForgetPage from './views/passwordForgetPage.js';
 import SignInPage from './views/signInPage.js';
 import SignUpPage from './views/signUpPage.js';
+import * as ROUTES from './constants/routes.js';
 
 class App extends Component {
     render(){
@@ -17,6 +18,13 @@ class App extends Component {
             <div className="App">
                 <Router>
                     <Nav />
+                    <Route exact path={ROUTES.LANDING} component={LandingPage} />
+                    <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+                    <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+                    <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+                    <Route path={ROUTES.HOME} component={HomePage} />
+                    <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+                    <Route path={ROUTES.ADMIN} component={AdminPage} />
                 </Router>
             </div>
         )
