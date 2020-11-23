6290f41b594344c2c34c562f4f4344b0dd5a923e
moontracer <themoontracer@gmail.com>
2020-11-23 11:00:33 -0500

    default firebase config added

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,3 +1,20 @@
+import app from 'firebase/app';
+
 const config = {
-    apiKey: 
+    apiKey: process.env.FIREBASE_APP_API_KEY,
+    authDomain: process.env.FIREBASE_APP_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_APP_DATABASE_URL,
+    projectID: process.env.FIREBASE_APP_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_APP_STORAGE_BUCKET,
+    messagingSenderID: process.env.FIREBASE_APP_MESSAGING_SENDER_ID,
+    appID: process.env.FIREBASE_APP_APPID,
+    measurementID: process.env.FIREBASE_APP_MEASUREMENT_ID
+};
+
+class Firebase {
+    constructor(){
+        app.initializeApp(config);
+    }
 }
+
+export default Firebase;
