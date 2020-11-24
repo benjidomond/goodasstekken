a4004fc131f39214ea8de828bb7fe73962f16b32
moontracer <themoontracer@gmail.com>
2020-11-24 14:48:29 -0500

    started sign up template

--- src/views/signUpPage.js
+++ src/views/signUpPage.js
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-class SignUpPage extends Component {
+import * as ROUTES from '../constants/routes.js';
+
+const SignUpPage = () => (
+    <div>
+        <h1>Sign Up</h1>
+        <SignUpForm />
+    </div>
+)
+
+class SignUpForm extends Component {
+    constructor(props){
+        super(props);
+    }
+    onSubmit = e => {
+
+    }
+    onChange = e => {
+
+    };
     render(){
         return (
-            <div>
-                <h1> Sign Up Page!</h1>
-            </div>
-        )
+            <form onSubmit={this.onSubmit}>
+
+            </form>
+        );
     }
 }
 
+const SignUpLink = () => (
+    <p>
+        Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+    </p>
+);
+
 export default SignUpPage;
+export { SignUpForm, SignUpLink };
