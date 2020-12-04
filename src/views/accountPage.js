15282b785dd5c2737eb6ac6d53c57905084d07b5
moontracer <themoontracer@gmail.com>
2020-12-04 09:29:59 -0500

    added account page logic

--- src/views/accountPage.js
+++ src/views/accountPage.js
@@ -1,13 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
+import PasswordChangeForm from "./passwordChange.js";
+import { PasswordForgetForm } from "./passwordForgetPage.js";
+import { AuthUserContext } from "../session";
 
-class AccountPage extends Component {
-    render(){
-        return (
-            <div>
-            <h1>Account Page component!</h1>
-            </div>
-        )
-    }
-}
+const AccountPage = () => (
+    <div>
+        <AuthUserContext.Consumer>
+            {authUser => authUser ? <AccountPageWithAuth /> : <AccountPageNoAuth />}
+        </AuthUserContext.Consumer>
+    </div>
+)
+
+const AccountPageWithAuth = () => (
+    <div>
+        <h1>Account Page</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+    </div>
+);
+
+const AccountPageNoAuth = () => (
+    <div>
+        <p>You're not currently signed in. Please sign in here: </p>
+    </div>
+)
 
 export default AccountPage;
